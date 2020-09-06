dd07735203aa11ffc88044c69c409044946a7922
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-06 15:33:00 +0530

    Initial Commit

--- src/VishalKeepsBoasting.java
+++ src/VishalKeepsBoasting.java
@@ -4,6 +4,7 @@ public class VishalKeepsBoasting {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+        String s = sc.nextLine();
         for(int i=0;i<t;i++){
             String str = sc.nextLine();
             if(isPalindrome(str)){
