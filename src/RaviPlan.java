2c34db400cdc4fc472dd0254a2d03c0675dea806
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-07 06:33:54 +0530

    Initial Commit

--- src/RaviPlan.java
+++ src/RaviPlan.java
@@ -1,25 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
+class RaviPlan {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        int n = fr.nextInt();
 
-public class RaviPlan {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
         int j=0;
         int k=0;
         int max=0;
-        String[] arr = br.readLine().split(" ");
         for(int i=0;i<n;i++){
-            int x = Integer.parseInt(arr[i]);
-            if (x==0){
+            int x = fr.nextInt();
+            if(x==0){
                 j++;
                 max = Math.max(j,max);
                 k=0;
-            }
-            else{
+            }else{
                 k++;
-                max=Math.max(k,max);
+                max = Math.max(k,max);
                 j=0;
             }
         }
