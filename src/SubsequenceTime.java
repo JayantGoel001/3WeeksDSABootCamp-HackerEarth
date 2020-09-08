e8ff0418961ea885e3f8bfa055d50c2045f08a62
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-08 20:02:58 +0530

    Initial Commit

--- src/SubsequenceTime.java
+++ src/SubsequenceTime.java
@@ -1,33 +1,32 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
 
-public class SubsequenceTime {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        String str = sc.nextLine();
-        for (int i = 0; i < t; i++) {
-            str = sc.nextLine();
-            ArrayList<String> arrayList = new ArrayList<>();
-            getSubSequence(str, arrayList, "");
+import java.io.*;
+import java.util.*;
+
+class SubsequenceTime {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t =Integer.parseInt(br.readLine());
+        for(int i=0;i<t;i++){
+            String str = br.readLine();
+            ArrayList<String> arrayList = new ArrayList<String>();
+            getSubSec(str,arrayList,"");
             Collections.sort(arrayList);
-
-            for (String ele : arrayList) {
+            for(String ele:arrayList){
                 System.out.println(ele);
             }
         }
-
     }
-
-    private static void getSubSequence(String str, ArrayList<String> arrayList,String subSec) {
-        if (str.isEmpty()){
-            arrayList.add(subSec);
+    public static void getSubSec(String str,ArrayList<String> arrayList,String subsec){
+        if(str.isEmpty()){
+            if(!arrayList.contains(subsec))
+            {
+                arrayList.add(subsec);
+            }
         }
         else{
             char ch = str.charAt(0);
-            getSubSequence(str.substring(1),arrayList,subSec+ch);
-            getSubSequence(str.substring(1),arrayList,subSec);
+            getSubSec(str.substring(1),arrayList,subsec);
+            getSubSec(str.substring(1),arrayList,subsec+ch);
         }
     }
 }
