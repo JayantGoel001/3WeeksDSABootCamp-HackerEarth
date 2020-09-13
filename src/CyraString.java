f4a17471143a98c82841d3ad5ae5faa72546aa56
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-13 12:55:00 +0530

    Initial Commit

--- src/CyraString.java
+++ src/CyraString.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class CyraString {
     public static void main(String[] args) throws IOException {
@@ -10,50 +12,21 @@ public class CyraString {
         int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
             String str = br.readLine();
-            HashSet<Character> hashSet = new HashSet<>();
+            HashMap<Character, Integer> hashMap = new HashMap<>();
             int len = str.length();
             for (int j = 0; j < len; j++) {
                 char x = str.charAt(j);
-                hashSet.add(x);
-            }
-            int maxLen = hashSet.size();
-
-            ArrayList<String> arrayList = new ArrayList<>();
-            generateSubset(new StringBuilder(str),arrayList,new StringBuilder(""),maxLen);
-            int count = 0;
-            for (String st:arrayList) {
-                if (st.length() == maxLen && getHashSet(st).size()==st.length()){
-                    count++;
+                if (!hashMap.containsKey(x)) {
+                    hashMap.put(x, 1);
+                } else {
+                    hashMap.replace(x, hashMap.get(x) + 1);
                 }
             }
-            System.out.println(count%(1000000000+7));
-        }
-    }
-
-    private static HashSet<Character> getHashSet(String st) {
-        HashSet<Character> hashSet = new HashSet<>();
-        for (int i =0;i<st.length();i++) {
-            hashSet.add(st.charAt(i));
-        }
-        return hashSet;
-    }
-
-    private static void generateSubset(StringBuilder str, ArrayList<String> arrayList, StringBuilder s,int maxLen) {
-        if(str.length()==0){
-            if (s.length()==maxLen) {
-                arrayList.add(s.toString());
+            int output = 1;
+            for (HashMap.Entry<Character, Integer> map : hashMap.entrySet()) {
+                output*=map.getValue()%(1000000000+7);
             }
-            return ;
+            System.out.println(output);
         }
-        char ch = str.charAt(0);
-        str.deleteCharAt(0);
-
-        s.append(ch);
-        generateSubset(str,arrayList,s,maxLen);
-        s.deleteCharAt(s.length()-1);
-
-        generateSubset(str,arrayList,s,maxLen);
-
-        str.insert(0,ch);
     }
 }
