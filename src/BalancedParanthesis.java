df95aa2917caad873cb8f4d16b7ee1a041ac5053
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-21 01:21:38 +0530

    Initial Commit

--- src/BalancedParanthesis.java
+++ src/BalancedParanthesis.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Stack;
 
-public class TestClass {
+public class BalancedParanthesis {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
