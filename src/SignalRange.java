f9736d515c1e797898f294125735670e17725a08
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-22 17:58:40 +0530

    Initial Commit

--- src/SignalRange.java
+++ src/SignalRange.java
@@ -43,22 +43,27 @@ public class SignalRange {
                 count[j]=0L;
             }
             Stack<Long> stack = new Stack<>();
+            HashMap<Long,Long> hashMap =new HashMap<>();
             for (int j = 0; j < n; j++) {
                 if (stack.isEmpty()) {
                     stack.push(ar[j]);
                     count[j]=1L;
+                    hashMap.put(ar[j],count[j]);
                 }else {
                     if (stack.peek()>=ar[j]){
                         stack.push(ar[j]);
                         count[j] += 1L;
+                        hashMap.put(ar[j],count[j]);
                     }else {
-                        int k =j;
-                        while (!stack.isEmpty() && stack.peek()<=ar[j]){
-                            count[j]+=count[--k];
+                        Long ele =stack.peek();
+                        while (!stack.isEmpty() && ele<=ar[j]){
+                            ele = stack.peek();
+                            count[j]+=hashMap.get(ele);
                             stack.pop();
                         }
                         stack.push(ar[j]);
                         count[j]+=1L;
+                        hashMap.put(ar[j],count[j]);
                     }
                 }
             }
