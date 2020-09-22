412da166d284121a5389e2418cce7f80c9fc8585
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-22 07:08:26 +0530

    Initial Commit

--- src/DiskTower.java
+++ src/DiskTower.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class DiskTower {
     static class FastReader{
@@ -28,25 +27,56 @@ public class DiskTower {
     }
     public static void main(String[] args) {
         FastReader fr = new FastReader();
-        Stack<Integer> stack = new Stack<>();
+        HashSet<Integer> priorityQueue = new HashSet<>();
         int n = fr.nextInt();
+        int[] ar = new int[n];
+
         for (int i = 0; i < n; i++) {
-            int x= fr.nextInt();
-            if (!stack.isEmpty()) {
-                if (x < stack.peek()) {
-                    while (!stack.isEmpty()) {
-                        System.out.print(stack.peek()+" ");
-                        stack.pop();
-                    }
-                }
+            ar[i] = fr.nextInt();
+            priorityQueue.add(ar[i]);
+        }
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            int index = getIndexOf(priorityQueue,ar[i]);
+//            System.out.println(index);
+            hashSet.add(ar[i]);
+            if (index == n-1){
+                printSet(hashSet,priorityQueue);
+                hashSet.clear();
+            }
+            if (i!=n-1) {
                 System.out.println();
             }
-            stack.push(x);
         }
-        while (!stack.isEmpty()){
-            System.out.print(stack.peek()+" ");
-            stack.pop();
+        if (!hashSet.isEmpty()){
+            printSet(hashSet,priorityQueue);
         }
-
     }
+
+    private static void printSet(HashSet<Integer> hashSet,HashSet<Integer> pq) {
+        int n  =hashSet.size();
+        int[] ar = new int[n];
+        Iterator<Integer> it = hashSet.iterator();
+        int i=0;
+        while (it.hasNext()){
+            ar[i++] = it.next();
+        }
+        for (int j = n-1; j >=0; j--) {
+            pq.remove(ar[j]);
+            System.out.print(ar[j]+" ");
+        }
+    }
+
+    private static int getIndexOf(HashSet<Integer> priorityQueue, int x) {
+        Iterator<Integer> it = priorityQueue.iterator();
+        int count=0;
+        while (it.hasNext()){
+            if (it.next().equals(x)){
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+
 }
